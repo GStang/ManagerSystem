c6896e715196ecd8e21dcf98a3e7a52f4ca960c0
DuZeming <1656499605@qq.com>
2018-05-01 20:03:04 +0800

    个人中心完成

--- app/src/main/java/com/swpuiot/managersystem/view/FirstActivity.java
+++ app/src/main/java/com/swpuiot/managersystem/view/FirstActivity.java
@@ -16,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -26,6 +27,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -66,11 +68,13 @@ public class FirstActivity extends AppCompatActivity {
     private NavigationView navigationView;
     ImageView menu;
     ClassAdapter adapter;
+    private MyUser myUser;
+    private TextView name;
+    private TextView note;
 
     public User getUser() {
         return user;
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,8 +90,21 @@ public class FirstActivity extends AppCompatActivity {
         if (user.getRole() == 1)
             getTeacherClass();//老师
 
+
         drawerLayout = (DrawerLayout) findViewById(R.id.activity_first);
         navigationView = (NavigationView) findViewById(R.id.nva);
+
+        assert navigationView != null;
+        MenuItem itemID = navigationView.getMenu().findItem(R.id.menu_id);
+        itemID.setTitle(MyUser.getUser().getId()+"");
+        MenuItem itemPhone = navigationView.getMenu().findItem(R.id.menu_phone);
+        itemPhone.setTitle(MyUser.getUser().getPhone()+"");
+        MenuItem itemEmail = navigationView.getMenu().findItem(R.id.menu_email);
+        itemEmail.setTitle(MyUser.getUser().getEmail());
+        name = (TextView) navigationView.getHeaderView(0).findViewById(R.id.tv_tv_first_name);
+        name.setText(MyUser.getUser().getName());
+
+
         menu= (ImageView) findViewById(R.id.main_menu);
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -130,17 +147,6 @@ public class FirstActivity extends AppCompatActivity {
         }
     }
 
-
-    private void initWindow() {//初始化窗口属性，让状态栏和导航栏透明
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            tintManager = new SystemBarTintManager(this);
-            int statusColor = Color.parseColor("#1976d2");
-            tintManager.setStatusBarTintColor(statusColor);
-            tintManager.setStatusBarTintEnabled(true);
-        }
-    }
 
     Retrofit retrofit = RetrofitUtil.getRetrofit();
     ClassService service = retrofit.create(ClassService.class);
